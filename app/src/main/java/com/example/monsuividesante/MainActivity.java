7602323ef0ac1a7e2e2f99bc8d77e4db577e4ba7
Dieunel MARCELIN <dieunel.marcelin@edu.univ-paris13.fr>
2024-06-13 20:07:41 +0200

    ajout page info_perso ?

--- app/src/main/java/com/example/monsuividesante/MainActivity.java
+++ app/src/main/java/com/example/monsuividesante/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.page_inscription), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.info_perso_inscription), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
