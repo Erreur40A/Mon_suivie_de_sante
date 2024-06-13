0b9cf62bcccaf1fecb0ceaa0c506b6965d9b7300
Jibril <douzijibril@gmail.com>
2024-06-13 16:06:28 +0200

    fin UI menu calories

--- app/src/main/java/com/example/monsuividesante/CaloriesActivity.java
+++ app/src/main/java/com/example/monsuividesante/CaloriesActivity.java
@@ -15,7 +15,7 @@ public class CaloriesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_calories);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.view_calories), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
