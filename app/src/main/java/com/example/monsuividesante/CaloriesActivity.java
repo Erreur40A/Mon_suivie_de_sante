b4336eecc5b971a7f03694bbc68e029f400e46d4
Jibril <douzijibril@gmail.com>
2024-06-17 16:21:32 +0200

    qlq amélioration sur la liste déroulante et la saisie

--- app/src/main/java/com/example/monsuividesante/CaloriesActivity.java
+++ app/src/main/java/com/example/monsuividesante/CaloriesActivity.java
@@ -2,10 +2,13 @@ package com.example.monsuividesante;
 
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -26,7 +29,7 @@ public class CaloriesActivity extends AppCompatActivity {
     private String calories_depense; //en kcal a afficher dans le rectangle vert
     private String calories_depense_reel; //en kcal a afficher dans le rectangle rouge
     private ConstraintLayout toolar;
-    private CaloriesActivity activity_calories;
+    private ConstraintLayout entrer_calorie_consomme;
     private ConstraintLayout liste_deroulante_choix_activite;
     private ArrayList<String> items_choix_activite;
     private AlertDialog pop_up_choix_activite;
@@ -59,7 +62,6 @@ public class CaloriesActivity extends AppCompatActivity {
 
         pop_up_duree_activite = null;
         pop_up_choix_activite = null;
-        activity_calories = this;
 
         TextView tmp_textView;
         tmp_textView = findViewById(R.id.calories_depense).findViewById(R.id.text_calorie_depense);
@@ -80,51 +82,51 @@ public class CaloriesActivity extends AppCompatActivity {
 
         ConstraintLayout tmp_CL = findViewById(R.id.demande_calorie_consomme);
 
-        ConstraintLayout enter_calorie_consomme = tmp_CL.findViewById(R.id.entrer_calorie_consomme);
-        bouton_edit_calories_consomme = enter_calorie_consomme.findViewById(R.id.bouton_modifications);
+        entrer_calorie_consomme = tmp_CL.findViewById(R.id.entrer_calorie_consomme);
+        bouton_edit_calories_consomme = entrer_calorie_consomme.findViewById(R.id.bouton_modifications);
 
-        bouton_edit_calories_consomme.setOnClickListener(this::setOnClickListenerCaloriesConsomme);
-        enter_calorie_consomme.setOnClickListener(this::setOnClickListenerCaloriesConsomme);
+        bouton_edit_calories_consomme.setOnClickListener(this::onClickListenerCaloriesConsomme);
+        entrer_calorie_consomme.setOnClickListener(this::onClickListenerCaloriesConsomme);
 
         bouton_calories_consome_ok = tmp_CL.findViewById(R.id.bouton_ok);
-        bouton_calories_consome_ok.setOnClickListener(this::setOnClickListenerBoutonConsommeOK);
+        bouton_calories_consome_ok.setOnClickListener(this::onClickListenerBoutonConsommeOK);
 
         tmp_CL = findViewById(R.id.demande_info_activite);
         bouton_activite_ok = tmp_CL.findViewById(R.id.bouton_ok_activite);
-        bouton_activite_ok.setOnClickListener(this::setOnClickListenerBoutonActiviteOK);
+        bouton_activite_ok.setOnClickListener(this::onClickListenerBoutonActiviteOK);
 
         bouton_pas = pas.findViewById(R.id.bouton_pas);
-        bouton_pas.setOnClickListener(this::setOnClickListenerBoutonPas);
+        bouton_pas.setOnClickListener(this::onClickListenerBoutonPas);
 
         bouton_mes_info = mes_info.findViewById(R.id.bouton_mes_info);
-        bouton_mes_info.setOnClickListener(this::setOnClickListenerBoutonMesInfo);
+        bouton_mes_info.setOnClickListener(this::onClickListenerBoutonMesInfo);
 
         bouton_calories = toolar.findViewById(R.id.calories).findViewById(R.id.bouton_calories);
-        bouton_calories.setOnClickListener(this::setOnClickListenerBoutonCalorie);
+        bouton_calories.setOnClickListener(this::onClickListenerBoutonCalorie);
 
         bouton_sommeil = sommeil.findViewById(R.id.bouton_sommeil);
-        bouton_sommeil.setOnClickListener(this::setOnClickListenerBoutonSommeil);
+        bouton_sommeil.setOnClickListener(this::onClickListenerBoutonSommeil);
 
         liste_deroulante_duree_activite = tmp_CL.findViewById(R.id.liste_deroulante_duree);
         bouton_liste_duree_activite = liste_deroulante_duree_activite.findViewById(R.id.bouton_liste_deroulante);
         /*La liste est à modifier
         Peut-être créer une classe Utilitaire qui récupère tout les durée présent dans la db*/
         items_duree_activite = new ArrayList<String>(Arrays.asList("00:15", "00:30", "1:00", "1:30", "2:00"));
-        liste_deroulante_duree_activite.setOnClickListener(this::setOnClickListenerActiviteDuree);
-        bouton_liste_duree_activite.setOnClickListener(this::setOnClickListenerActiviteDuree);
+        liste_deroulante_duree_activite.setOnClickListener(this::onClickListenerActiviteDuree);
+        bouton_liste_duree_activite.setOnClickListener(this::onClickListenerActiviteDuree);
 
         liste_deroulante_choix_activite = tmp_CL.findViewById(R.id.liste_deroulante_activite);
         bouton_liste_choix_activite = liste_deroulante_choix_activite.findViewById(R.id.bouton_liste_deroulante);
         /*La liste est à modifier
         Peut-être créer une classe Utilitaire qui récupère tout les activité présent dans la db*/
         items_choix_activite = new ArrayList<String>(Arrays.asList("activité1", "activité2", "activité3", "activité4", "activité5"));
-        liste_deroulante_choix_activite.setOnClickListener(this::setOnClickListenerChoixActivite);
-        bouton_liste_choix_activite.setOnClickListener(this::setOnClickListenerChoixActivite);
+        liste_deroulante_choix_activite.setOnClickListener(this::onClickListenerChoixActivite);
 
+        bouton_liste_choix_activite.setOnClickListener(this::onClickListenerChoixActivite);
     }
 
-    public void setOnClickListenerCaloriesConsomme(View view){
-        AlertDialog.Builder pop_up_calorie_consomme = new AlertDialog.Builder(activity_calories, R.style.PopUpArrondi);
+    public void onClickListenerCaloriesConsomme(View view){
+        AlertDialog.Builder pop_up_calorie_consomme = new AlertDialog.Builder(this, R.style.PopUpArrondi);
 
         pop_up_calorie_consomme.setView(R.layout.pop_up_calorie);
 
@@ -132,29 +134,29 @@ public class CaloriesActivity extends AppCompatActivity {
         pop_up.show();
 
         Button bouton_ok = pop_up.findViewById(R.id.bouton_ok);
-        assert bouton_ok != null;
         bouton_ok.setOnClickListener(v -> {
-            /*A compléter*/
+            TextView choix = entrer_calorie_consomme.findViewById(R.id.calorie_consome);
+            EditText saisie = pop_up.findViewById(R.id.saisie_user);
+
+            String affichage = saisie.getText() + " kcal";
+
+            choix.setText(affichage);
+            choix.setTextColor(Color.BLACK);
 
             pop_up.dismiss();
         });
 
         Button bouton_annuler = pop_up.findViewById(R.id.bouton_annuler);
-        assert bouton_annuler != null;
-        bouton_annuler.setOnClickListener(v -> {
-            /*A compléter*/
-
-            pop_up.dismiss();
-        });
+        bouton_annuler.setOnClickListener(v -> { pop_up.dismiss(); });
     }
 
-    public void setOnClickListenerActiviteDuree(View view){
-        AlertDialog.Builder pop_up_activite_dure_builder = new AlertDialog.Builder(activity_calories, R.style.PopUpArrondi);
+    public void onClickListenerActiviteDuree(View view){
+        AlertDialog.Builder pop_up_activite_dure_builder = new AlertDialog.Builder(this, R.style.PopUpArrondi);
 
         LayoutInflater inflater = getLayoutInflater();
         View view_pop_up = inflater.inflate(R.layout.pop_up_duree_activite, null);
 
-        LinearLayout liste_deroulante = view_pop_up.findViewById(R.id.items_duree_activite_liste);
+        LinearLayout liste_deroulante = view_pop_up.findViewById(R.id.liste_duree_activite);
 
         for (String item : items_duree_activite) {
             View view_item = inflater.inflate(R.layout.background_item_liste_deroulante, null, false);
@@ -165,7 +167,7 @@ public class CaloriesActivity extends AppCompatActivity {
             textViewItem.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    setOnClickListenerItemListeDeroulante(view);
+                    onClickListenerItemListeDeroulante(view, liste_deroulante_duree_activite);
 
                     if(pop_up_duree_activite != null && pop_up_duree_activite.isShowing())
                         pop_up_duree_activite.dismiss();
@@ -181,13 +183,13 @@ public class CaloriesActivity extends AppCompatActivity {
         pop_up_duree_activite.show();
     }
 
-    public void setOnClickListenerChoixActivite(View view){
-        AlertDialog.Builder pop_up_activite_choix_builder = new AlertDialog.Builder(activity_calories, R.style.PopUpArrondi);
+    public void onClickListenerChoixActivite(View view){
+        AlertDialog.Builder pop_up_activite_choix_builder = new AlertDialog.Builder(this, R.style.PopUpArrondi);
 
         LayoutInflater inflater = getLayoutInflater();
         View view_pop_up = inflater.inflate(R.layout.pop_up_choix_activite, null);
 
-        LinearLayout liste_deroulante = view_pop_up.findViewById(R.id.items_choix_activite_liste);
+        LinearLayout liste_deroulante = view_pop_up.findViewById(R.id.liste_choix_activite);
 
         for (String item : items_choix_activite) {
             View view_item = inflater.inflate(R.layout.background_item_liste_deroulante, null, false);
@@ -198,7 +200,7 @@ public class CaloriesActivity extends AppCompatActivity {
             textViewItem.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    setOnClickListenerItemListeDeroulante(view);
+                    onClickListenerItemListeDeroulante(view, liste_deroulante_choix_activite);
 
                     if(pop_up_choix_activite != null && pop_up_choix_activite.isShowing())
                         pop_up_choix_activite.dismiss();
@@ -214,45 +216,46 @@ public class CaloriesActivity extends AppCompatActivity {
         pop_up_choix_activite.show();
     }
 
-    public void setOnClickListenerItemListeDeroulante(View view){
-        Toast.makeText(getApplicationContext(), "test", Toast.LENGTH_SHORT).show();
+    public void onClickListenerItemListeDeroulante(View view, ViewGroup liste_deroulante){
+        String choix = (String) ((TextView) view).getText();
+
+        TextView explication = liste_deroulante.findViewById(R.id.explication_liste_déroulante);
+
+        explication.setText(choix);
+        explication.setTextColor(Color.BLACK);
     }
 
-    public void setOnClickListenerBoutonPas(View view){
+    public void onClickListenerBoutonPas(View view){
         /*Modifier MainActivity.class par la classe java de l'activity Pas)*/
         Intent intent = new Intent(CaloriesActivity.this, MainActivity.class);
         startActivity(intent);
     }
 
-    public void setOnClickListenerBoutonCalorie(View view){
-        /*Modifier MainActivity.class par la classe java de l'activity Pas)*/
+    public void onClickListenerBoutonCalorie(View view){
+        /*Soit on supprime ce listener soit on le garde*/
         Intent intent = new Intent(CaloriesActivity.this, CaloriesActivity.class);
         startActivity(intent);
     }
 
-    public void setOnClickListenerBoutonMesInfo(View view){
-        /*Modifier MainActivity.class par la classe java de l'activity Pas)*/
+    public void onClickListenerBoutonMesInfo(View view){
+        /*Modifier MainActivity.class par la classe java de l'activity Mes informations)*/
         Intent intent = new Intent(CaloriesActivity.this, MainActivity.class);
         startActivity(intent);
     }
 
-    public void setOnClickListenerBoutonSommeil(View view){
-        /*Modifier MainActivity.class par la classe java de l'activity Pas)*/
+    public void onClickListenerBoutonSommeil(View view){
+        /*Modifier MainActivity.class par la classe java de l'activity Sommeil)*/
         Intent intent = new Intent(CaloriesActivity.this, MainActivity.class);
         startActivity(intent);
     }
 
-    public void setOnClickListenerBoutonConsommeOK(View view){
+    public void onClickListenerBoutonConsommeOK(View view){
         /*A MODIFIER*/
         Toast.makeText(getApplicationContext(), "bouton ok consomme clique", Toast.LENGTH_SHORT).show();
     }
 
-    public void setOnClickListenerBoutonActiviteOK(View view){
+    public void onClickListenerBoutonActiviteOK(View view){
         /*A MODIFIER*/
-        Toast.makeText(getApplicationContext(), "bouton ok activite clique", Toast.LENGTH_SHORT).show();
-    }
-
-    public void setOnSelectedItemSpinner(View view){
         Toast.makeText(getApplicationContext(), "bouton ok activite clique", Toast.LENGTH_SHORT).show();
     }
 }
