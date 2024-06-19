5bca6f61d8a9d87c4ecec042b798a5c8ed618094
Jibril <douzijibril@gmail.com>
2024-06-19 21:33:38 +0200

    le logo est le logo de l'app + fin notification + modif layout sommeil_activity

--- app/src/main/java/com/example/monsuividesante/SommeilActivity.java
+++ app/src/main/java/com/example/monsuividesante/SommeilActivity.java
@@ -1,13 +1,18 @@
 package com.example.monsuividesante;
 
+import android.app.AlarmManager;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AlertDialog;
@@ -30,6 +35,9 @@ public class SommeilActivity extends AppCompatActivity {
     private ImageButton bouton_sommeil;
     private ImageButton bouton_heure_coucher;
     private ImageButton bouton_heure_reveil;
+    private ImageButton bouton_heure_coucher_reel;
+    private ImageButton bouton_heure_reveil_reel;
+    private boolean autorisation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,6 +77,35 @@ public class SommeilActivity extends AppCompatActivity {
 
         bouton_heure_reveil = objectif_sommeil.findViewById(R.id.bouton_reveil);
         bouton_heure_reveil.setOnClickListener(this::onClickListenerHeureReveil);
+
+        bouton_heure_coucher_reel = objectif_sommeil.findViewById(R.id.bouton_coucher_reel);
+        bouton_heure_coucher_reel.setOnClickListener(this::onClickListenerHeureCoucherReel);
+
+        bouton_heure_reveil_reel = objectif_sommeil.findViewById(R.id.bouton_reveil_reel);
+        bouton_heure_reveil_reel.setOnClickListener(this::onClickListenerHeureReveilReel);
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        autorisation=verifierAutorisation();
+    }
+
+    public boolean verifierAutorisation(){
+        /*Pour les version Android 12 ou plus il faut demander l'autorisation d'utiliser l'alarme exact*/
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+            if (alarmManager != null && !alarmManager.canScheduleExactAlarms()) {
+                Toast.makeText(this, Rappel.explication, Toast.LENGTH_LONG).show();
+
+                Intent intent = new Intent(Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM);
+                startActivity(intent);
+
+                return false;
+            }
+        }
+
+        return true;
     }
 
     public void onClickListenerHeureCoucher(View view){
@@ -85,8 +122,15 @@ public class SommeilActivity extends AppCompatActivity {
             assert saisie != null;
 
             TextView heure_coucher = objectif_sommeil.findViewById(R.id.objectif_sommeil).findViewById(R.id.heure_coucher);
-            String affichage = String.valueOf(saisie.getText());
 
+            String affichage = String.valueOf(saisie.getText());
+            /*Vérifier si 'affichage' correspond à une vrai heure
+            * par exemple :
+            * 08:30 validé
+            * 2134, lkjhw et 30:00 non validé*/
+            String[] horaire = affichage.split(":");
+
+            Rappel.setRappel(this, autorisation, Integer.parseInt(horaire[0]), Integer.parseInt(horaire[1]));
             heure_coucher.setText(affichage);
 
             pop_up.dismiss();
@@ -112,8 +156,74 @@ public class SommeilActivity extends AppCompatActivity {
 
             TextView heure_reveil = objectif_sommeil.findViewById(R.id.objectif_sommeil).findViewById(R.id.heure_reveil);
             String affichage = String.valueOf(saisie.getText());
+            /*Vérifier si 'affichage' correspond à une vrai heure
+             * par exemple :
+             * 08:30 validé
+             * 2134, lkjhw et 30:00 non validé*/
 
             heure_reveil.setText(affichage);
+
+            pop_up.dismiss();
+        });
+
+        Button bouton_annuler = pop_up.findViewById(R.id.bouton_annuler);
+        assert bouton_annuler != null;
+        bouton_annuler.setOnClickListener(v -> { pop_up.dismiss(); });
+    }
+
+    public void onClickListenerHeureCoucherReel(View view){
+        AlertDialog.Builder pop_up_objectif_coucher = new AlertDialog.Builder(this ,R.style.PopUpArrondi);
+        pop_up_objectif_coucher.setView(R.layout.pop_up_heure_coucher_reel);
+
+        AlertDialog pop_up = pop_up_objectif_coucher.create();
+        pop_up.show();
+
+        Button bouton_ok = pop_up.findViewById(R.id.bouton_ok);
+        assert bouton_ok != null;
+        bouton_ok.setOnClickListener(v -> {
+            EditText saisie = pop_up.findViewById(R.id.saisie_user);
+            assert saisie != null;
+
+            TextView heure_reveil = objectif_sommeil.findViewById(R.id.objectif_sommeil).findViewById(R.id.heure_coucher_reel);
+            String affichage = String.valueOf(saisie.getText());
+            /*Vérifier si 'affichage' correspond à une vrai heure
+             * par exemple :
+             * 08:30 validé
+             * 2134, lkjhw et 30:00 non validé*/
+            heure_reveil.setText(affichage);
+
+            /*Mettre 'affichage' dans la DB*/
+
+            pop_up.dismiss();
+        });
+
+        Button bouton_annuler = pop_up.findViewById(R.id.bouton_annuler);
+        assert bouton_annuler != null;
+        bouton_annuler.setOnClickListener(v -> { pop_up.dismiss(); });
+    }
+
+    public void onClickListenerHeureReveilReel(View view){
+        AlertDialog.Builder pop_up_objectif_coucher = new AlertDialog.Builder(this ,R.style.PopUpArrondi);
+        pop_up_objectif_coucher.setView(R.layout.pop_up_heure_reveil_reel);
+
+        AlertDialog pop_up = pop_up_objectif_coucher.create();
+        pop_up.show();
+
+        Button bouton_ok = pop_up.findViewById(R.id.bouton_ok);
+        assert bouton_ok != null;
+        bouton_ok.setOnClickListener(v -> {
+            EditText saisie = pop_up.findViewById(R.id.saisie_user);
+            assert saisie != null;
+
+            TextView heure_reveil = objectif_sommeil.findViewById(R.id.objectif_sommeil).findViewById(R.id.heure_reveil_reel);
+            String affichage = String.valueOf(saisie.getText());
+            /*Vérifier si 'affichage' correspond à une vrai heure
+             * par exemple :
+             * 08:30 validé
+             * 2134, lkjhw et 30:00 non validé*/
+            heure_reveil.setText(affichage);
+
+            /*Mettre 'affichage' dans la DB*/
 
             pop_up.dismiss();
         });
