8e8a4968535c82533cd2773d1b47736d387d271c
Jibril <douzijibril@gmail.com>
2024-06-20 21:39:49 +0200

    ajout qlq methode dans Regex

--- app/src/main/java/com/example/monsuividesante/Regex.java
+++ app/src/main/java/com/example/monsuividesante/Regex.java
@@ -1,9 +1,15 @@
 package com.example.monsuividesante;
 
 public class Regex {
-    public static boolean isValideHeure(String heure){
+    public static boolean isValideHeureSaisie(String heure){
         String expr = "([0-1][0-9]|2[0-3]):[0-5][0-9]";
 
         return heure.matches(expr);
+    }
+
+    public static boolean isValideCalorieSaisie(String calorie){
+        if(calorie.matches("[0-9]*"))
+            return Integer.parseInt(calorie)<=2000;
+        return false;
     }
 }
