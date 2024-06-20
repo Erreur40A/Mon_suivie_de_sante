72ac556b6b4d3f84ae8813a3191b51e7f24785ba
Matthias <124595526+matthyass@users.noreply.github.com>
2024-06-20 17:55:21 +0200

    fichier modification database

---
+++ main/java/com/example/monsuividesante/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.monsuividesante;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String id = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        final DatabaseAccess db = DatabaseAccess.getInstance(getApplicationContext());
+        db.open();
+
+        id = db.gettkt("1");
+        TextView textView_id = (TextView) findViewById(R.id.ici);
+        textView_id.setText(id);
+
+        db.close();
+    }
+}
