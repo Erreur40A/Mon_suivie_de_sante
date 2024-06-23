69a550e1763e6d672b067d35c856803e13b547f5
Jibril <douzijibril@gmail.com>
2024-06-24 00:13:53 +0200

    listener nb pas

--- app/src/main/java/com/example/monsuividesante/NombreDePasActivity.java
+++ app/src/main/java/com/example/monsuividesante/NombreDePasActivity.java
@@ -4,11 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import androidx.activity.EdgeToEdge;
+import android.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 
@@ -16,67 +20,145 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class NombreDePasActivity extends AppCompatActivity {
 
-    private Button buttonStylo, buttonInfos, buttonPas, buttonCalories, buttonSommeil;
-    private TextView textViewObjectifs;
+    private TextView pas_journalier_fait, pas_hebdomadaire_fait, pas_mensuelle_fait;
+    private TextView objectif_journalier, objectif_hebdomadaire, objectif_mensuelle;
+    private TextView pourcent_journaier, pourcent_hebdomadaire, pourcent_mensuelle;
+    private ImageButton bouton_journalier, bouton_hebdomadaire, bouton_mensuelle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_nombre_de_pas);
-
-        // Initialiser les composants UI
-        buttonStylo = findViewById(R.id.button_stylo);
-        buttonInfos = findViewById(R.id.button_infos);
-        buttonPas = findViewById(R.id.button_pas);
-        buttonCalories = findViewById(R.id.button_calories);
-        buttonSommeil = findViewById(R.id.button_sommeil);
-        textViewObjectifs = findViewById(R.id.text_view_objectifs);
-
-        // Listener pour le bouton Stylo
-        buttonStylo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Afficher la zone de texte pour entrer l'objectif
-                textViewObjectifs.setVisibility(View.VISIBLE);
-                Toast.makeText(NombreDePasActivity.this, "Entrez votre objectif.", Toast.LENGTH_SHORT).show();
-            }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activity_nb_pas), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
         });
 
-        // Listener pour le bouton Mes Informations
-        buttonInfos.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Changer de menu ou d'activité
-                Intent intent = new Intent(NombreDePasActivity.this, NombreDePasActivity.class);
-                startActivity(intent);
-            }
+        ConstraintLayout toolbar = findViewById(R.id.toolbar);
+        ImageButton pas = toolbar.findViewById(R.id.pas).findViewById(R.id.bouton_pas);
+        ImageButton calories = toolbar.findViewById(R.id.calories).findViewById(R.id.bouton_calories);
+        ImageButton mes_info = toolbar.findViewById(R.id.mes_info).findViewById(R.id.bouton_mes_info);
+        ImageButton sommeil = toolbar.findViewById(R.id.sommeil).findViewById(R.id.bouton_sommeil);
+
+        pas.setOnClickListener(this::onClickListenerBoutonPas);
+        mes_info.setOnClickListener(this::onClickListenerBoutonMesInfo);
+        calories.setOnClickListener(this::onClickListenerBoutonCalorie);
+        sommeil.setOnClickListener(this::onClickListenerBoutonSommeil);
+
+        ConstraintLayout journ = findViewById(R.id.objectif_journalier).findViewById(R.id.rec);
+        ConstraintLayout hebd = findViewById(R.id.objectif_hebdomadaire).findViewById(R.id.rec1);
+        ConstraintLayout mens = findViewById(R.id.objectif_mensuelle).findViewById(R.id.rec2);
+
+        bouton_journalier = journ.findViewById(R.id.bouton_journalier);
+        bouton_hebdomadaire = hebd.findViewById(R.id.bouton_hebdomadaire);
+        bouton_mensuelle = mens.findViewById(R.id.bouton_mensuelle);
+
+        objectif_journalier = journ.findViewById(R.id.val_objectif_journalier);
+        objectif_hebdomadaire = hebd.findViewById(R.id.val_objectif_hebdomadaire);
+        objectif_mensuelle = mens.findViewById(R.id.val_objectif_mensuelle);
+
+        pas_journalier_fait = findViewById(R.id.objectif_journalier).findViewById(R.id.nb_pas_journalier);
+        pas_hebdomadaire_fait = findViewById(R.id.objectif_hebdomadaire).findViewById(R.id.nb_pas_hebdomadaire);
+        pas_mensuelle_fait = findViewById(R.id.objectif_mensuelle).findViewById(R.id.nb_pas_mensuelle);
+
+        bouton_journalier.setOnClickListener(this::onClickListenerObjectifJournalier);
+        bouton_mensuelle.setOnClickListener(this::onClickListenerObjectifMensuelle);
+        bouton_hebdomadaire.setOnClickListener(this::onClickListenerObjectifHebdomadaire);
+    }
+
+    public void onClickListenerObjectifJournalier(View view){
+        AlertDialog.Builder pop_up_objectif_journalier = new AlertDialog.Builder(this, R.style.PopUpArrondi);
+
+        pop_up_objectif_journalier.setView(R.layout.pop_up_obj_jour);
+
+        AlertDialog pop_up = pop_up_objectif_journalier.create();
+        pop_up.show();
+
+        Button bouton_ok = pop_up.findViewById(R.id.bouton_ok);
+        bouton_ok.setOnClickListener(v -> {
+            TextView choix = objectif_journalier.findViewById(R.id.val_objectif_journalier);
+            EditText saisie = pop_up.findViewById(R.id.saisie_user);
+            String affichage = saisie.getText().toString();
+
+            choix.setText(affichage);
+            pop_up.dismiss();
         });
 
-        // Listener pour le bouton Pas
-        buttonPas.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Changer de menu ou d'activité
-                Toast.makeText(NombreDePasActivity.this, "Affichage des pas.", Toast.LENGTH_SHORT).show();
-            }
+        Button bouton_annuler = pop_up.findViewById(R.id.bouton_annuler);
+        bouton_annuler.setOnClickListener(v -> pop_up.dismiss());
+    }
+
+    public void onClickListenerObjectifHebdomadaire(View view){
+        AlertDialog.Builder pop_up_objectif_hedomadaire = new AlertDialog.Builder(this, R.style.PopUpArrondi);
+
+        pop_up_objectif_hedomadaire.setView(R.layout.pop_up_obj_hebd);
+
+        AlertDialog pop_up = pop_up_objectif_hedomadaire.create();
+        pop_up.show();
+
+        Button bouton_ok = pop_up.findViewById(R.id.bouton_ok);
+        bouton_ok.setOnClickListener(v -> {
+            TextView choix = objectif_hebdomadaire.findViewById(R.id.val_objectif_hebdomadaire);
+            EditText saisie = pop_up.findViewById(R.id.saisie_user);
+            String affichage = saisie.getText().toString();
+
+            choix.setText(affichage);
+            pop_up.dismiss();
         });
 
-        // Listener pour le bouton Calories
-        buttonCalories.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Changer de menu ou d'activité
-                Toast.makeText(NombreDePasActivity.this, "Affichage des calories.", Toast.LENGTH_SHORT).show();
-            }
+        Button bouton_annuler = pop_up.findViewById(R.id.bouton_annuler);
+        bouton_annuler.setOnClickListener(v -> pop_up.dismiss());
+    }
+
+    public void onClickListenerObjectifMensuelle(View view){
+        AlertDialog.Builder pop_up_objectif_mensuelle = new AlertDialog.Builder(this, R.style.PopUpArrondi);
+
+        pop_up_objectif_mensuelle.setView(R.layout.pop_up_obj_mens);
+
+        AlertDialog pop_up = pop_up_objectif_mensuelle.create();
+        pop_up.show();
+
+        Button bouton_ok = pop_up.findViewById(R.id.bouton_ok);
+        bouton_ok.setOnClickListener(v -> {
+            TextView choix = objectif_mensuelle.findViewById(R.id.val_objectif_mensuelle);
+            EditText saisie = pop_up.findViewById(R.id.saisie_user);
+            String affichage = saisie.getText().toString();
+
+            choix.setText(affichage);
+            pop_up.dismiss();
         });
 
-        // Listener pour le bouton Sommeil
-        buttonSommeil.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Changer de menu ou d'activité
-                Toast.makeText(NombreDePasActivity.this, "Affichage du sommeil.", Toast.LENGTH_SHORT).show();
-            }
-        });
+        Button bouton_annuler = pop_up.findViewById(R.id.bouton_annuler);
+        bouton_annuler.setOnClickListener(v -> pop_up.dismiss());
+    }
+
+    public void onClickListenerBoutonPas(View view){
+        /*Modifier MainActivity.class par la classe java de l'activity Pas)*/
+        Intent intent = new Intent(NombreDePasActivity.this, NombreDePasActivity.class);
+        //intent.putExtra("user", user);
+        startActivity(intent);
+    }
+
+    public void onClickListenerBoutonCalorie(View view){
+        /*Soit on supprime ce listener soit on le garde*/
+        Intent intent = new Intent(NombreDePasActivity.this, MainActivity.class);
+        //intent.putExtra("user", user);
+        startActivity(intent);
+    }
+
+    public void onClickListenerBoutonMesInfo(View view){
+        /*Modifier MainActivity.class par la classe java de l'activity Mes informations)*/
+        Intent intent = new Intent(NombreDePasActivity.this, MainActivity.class);
+        //intent.putExtra("user", user);
+        startActivity(intent);
+    }
+
+    public void onClickListenerBoutonSommeil(View view){
+        /*Modifier MainActivity.class par la classe java de l'activity Sommeil)*/
+        Intent intent = new Intent(NombreDePasActivity.this, MainActivity.class);
+        //intent.putExtra("user", user);
+        startActivity(intent);
     }
 }
