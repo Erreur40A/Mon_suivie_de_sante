067a923eaa65b13bab173db9f39debe5a61ef9bf
Jibril <douzijibril@gmail.com>
2024-06-24 17:53:08 +0200

    ajout pop-up mes infos

--- app/src/main/java/com/example/monsuividesante/ActivityMesInformations.java
+++ app/src/main/java/com/example/monsuividesante/ActivityMesInformations.java
@@ -1,14 +1,21 @@
 package com.example.monsuividesante;
 
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class ActivityMesInformations extends AppCompatActivity {
@@ -16,8 +23,10 @@ public class ActivityMesInformations extends AppCompatActivity {
 
     private DatabaseAccess databaseAccess;
     private DatabaseOpenhelper db_helper;
-    private EditText editName, editFirstName, editAge, editWeight, editHeight;
-    private TextView bienvenue;
+    private ConstraintLayout layout_nom, layout_prenom, layout_age, layout_taille, layout_poids;
+    //private EditText editName, editFirstName, editAge, editWeight, editHeight;
+    private TextView bienvenue, nom, prenom, age, taille, poids;
+    //private User user;
 
     //a modifier
     private int user_id = 1;
@@ -34,6 +43,9 @@ public class ActivityMesInformations extends AppCompatActivity {
             return insets;
         });
 
+        //enlever le commentaire lors de la fusion
+        //user=getIntent().getSerializableExtra("user");
+
         // Initialiser l'accès à la base de données
         databaseAccess = DatabaseAccess.getInstance(this);
         db_helper = new DatabaseOpenhelper(this);
@@ -43,8 +55,36 @@ public class ActivityMesInformations extends AppCompatActivity {
         db_helper.addLigneIdentite(user_id);
         /*--------------------------------*/
 
-        // Initialiser les vues
+        /*------------------Avec pop-up-------------------*/
+
+        layout_age = findViewById(R.id.layout_age);
+        layout_nom = findViewById(R.id.layout_nom);
+        layout_prenom = findViewById(R.id.layout_prenom);
+        layout_taille = findViewById(R.id.layout_taille);
+        layout_poids = findViewById(R.id.layout_poids);
+
+        age = layout_age.findViewById(R.id.age);
+        taille = layout_taille.findViewById(R.id.taille);
+        nom = layout_nom.findViewById(R.id.nom);
+        prenom = layout_prenom.findViewById(R.id.prenom);
+        poids = layout_poids.findViewById(R.id.poids);
+
         bienvenue = findViewById(R.id.text_welcome);
+
+        // Charger les informations utilisateur
+        loadUserInfo();
+
+        layout_age.setOnClickListener(this::onClickListenerAge);
+        layout_nom.setOnClickListener(this::onClickListenerNom);
+        layout_prenom.setOnClickListener(this::onClickListenerPrenom);
+        layout_taille.setOnClickListener(this::onClickListenerTaille);
+        layout_poids.setOnClickListener(this::onClickListenerPoids);
+
+        /*------------------------------------------------*/
+
+        /*-------Sans pop-up--------------------------*/
+        // Initialiser les vues
+        /*bienvenue = findViewById(R.id.text_welcome);
         editName = findViewById(R.id.edit_name);
         editFirstName = findViewById(R.id.edit_firstname);
         editAge = findViewById(R.id.edit_age);
@@ -56,7 +96,168 @@ public class ActivityMesInformations extends AppCompatActivity {
 
         // Ajouter les listeners pour les champs de texte
         //a modfifier Matthias à deja fait
-        addTextWatchers();
+        addTextWatchers();*/
+        /*---------------------------------------------*/
+    }
+
+    public void onClickListenerNom(View view){
+        AlertDialog.Builder pop_up_calorie_consomme = new AlertDialog.Builder(this, R.style.PopUpArrondi);
+
+        pop_up_calorie_consomme.setView(R.layout.pop_up_nom);
+
+        AlertDialog pop_up = pop_up_calorie_consomme.create();
+        pop_up.show();
+
+        Button bouton_ok = pop_up.findViewById(R.id.bouton_ok);
+        bouton_ok.setOnClickListener(v -> {
+            EditText saisie = pop_up.findViewById(R.id.saisie_user);
+            String affichage = saisie.getText().toString();
+
+            if(Regex.estNomPrenomValide(affichage)) {
+                nom.setText(affichage);
+                nom.setTextColor(Color.BLACK);
+            }
+
+            pop_up.dismiss();
+        });
+
+        Button bouton_annuler = pop_up.findViewById(R.id.bouton_annuler);
+        bouton_annuler.setOnClickListener(v -> pop_up.dismiss());
+    }
+
+    public void onClickListenerPrenom(View view){
+        AlertDialog.Builder pop_up_calorie_consomme = new AlertDialog.Builder(this, R.style.PopUpArrondi);
+
+        pop_up_calorie_consomme.setView(R.layout.pop_up_prenom);
+
+        AlertDialog pop_up = pop_up_calorie_consomme.create();
+        pop_up.show();
+
+        Button bouton_ok = pop_up.findViewById(R.id.bouton_ok);
+        bouton_ok.setOnClickListener(v -> {
+            EditText saisie = pop_up.findViewById(R.id.saisie_user);
+            String affichage = saisie.getText().toString();
+
+            if(Regex.estNomPrenomValide(affichage)) {
+                prenom.setText(affichage);
+                prenom.setTextColor(Color.BLACK);
+                String bienv = "Bienvenue " + affichage;
+                bienvenue.setText(bienv);
+            }
+
+            pop_up.dismiss();
+        });
+
+        Button bouton_annuler = pop_up.findViewById(R.id.bouton_annuler);
+        bouton_annuler.setOnClickListener(v -> pop_up.dismiss());
+    }
+
+    public void onClickListenerPoids(View view){
+        AlertDialog.Builder pop_up_calorie_consomme = new AlertDialog.Builder(this, R.style.PopUpArrondi);
+
+        pop_up_calorie_consomme.setView(R.layout.pop_up_poids);
+
+        AlertDialog pop_up = pop_up_calorie_consomme.create();
+        pop_up.show();
+
+        Button bouton_ok = pop_up.findViewById(R.id.bouton_ok);
+        bouton_ok.setOnClickListener(v -> {
+            EditText saisie = pop_up.findViewById(R.id.saisie_user);
+            String affichage = saisie.getText().toString();
+
+            if(Regex.estPoidsValide(affichage)) {
+                poids.setText(affichage);
+                poids.setTextColor(Color.BLACK);
+            }
+
+            pop_up.dismiss();
+        });
+
+        Button bouton_annuler = pop_up.findViewById(R.id.bouton_annuler);
+        bouton_annuler.setOnClickListener(v -> pop_up.dismiss());
+    }
+
+    public void onClickListenerAge(View view){
+        AlertDialog.Builder pop_up_calorie_consomme = new AlertDialog.Builder(this, R.style.PopUpArrondi);
+
+        pop_up_calorie_consomme.setView(R.layout.pop_up_age);
+
+        AlertDialog pop_up = pop_up_calorie_consomme.create();
+        pop_up.show();
+
+        Button bouton_ok = pop_up.findViewById(R.id.bouton_ok);
+        bouton_ok.setOnClickListener(v -> {
+            EditText saisie = pop_up.findViewById(R.id.saisie_user);
+            String affichage = saisie.getText().toString();
+
+            if(Regex.estAgeValide(affichage)) {
+                age.setText(affichage);
+                age.setTextColor(Color.BLACK);
+            }
+
+            pop_up.dismiss();
+        });
+
+        Button bouton_annuler = pop_up.findViewById(R.id.bouton_annuler);
+        bouton_annuler.setOnClickListener(v -> pop_up.dismiss());
+    }
+
+    public void onClickListenerTaille(View view){
+        AlertDialog.Builder pop_up_calorie_consomme = new AlertDialog.Builder(this, R.style.PopUpArrondi);
+
+        pop_up_calorie_consomme.setView(R.layout.pop_up_taille);
+
+        AlertDialog pop_up = pop_up_calorie_consomme.create();
+        pop_up.show();
+
+        Button bouton_ok = pop_up.findViewById(R.id.bouton_ok);
+        bouton_ok.setOnClickListener(v -> {
+            EditText saisie = pop_up.findViewById(R.id.saisie_user);
+            String affichage = saisie.getText().toString();
+
+            if(Regex.estTailleValide(affichage)) {
+                taille.setText(affichage);
+                taille.setTextColor(Color.BLACK);
+            }
+
+            pop_up.dismiss();
+        });
+
+        Button bouton_annuler = pop_up.findViewById(R.id.bouton_annuler);
+        bouton_annuler.setOnClickListener(v -> pop_up.dismiss());
+    }
+
+    /*Listener des boutons de la toolbar*/
+    public void onClickListenerBoutonPas(View view){
+        /*Modifier MainActivity.class par la classe java de l'activity Pas)*/
+        Intent intent = new Intent(ActivityMesInformations.this, MainActivity.class);
+        /*enlever le // du dessous*/
+        //intent.putExtra("user", user);
+        startActivity(intent);
+    }
+
+    public void onClickListenerBoutonCalorie(View view){
+        /*Modifier MainActivity.class par la classe java de l'activity Calories*/
+        Intent intent = new Intent(ActivityMesInformations.this, MainActivity.class);
+        /*enlever le // du dessous*/
+        //intent.putExtra("user", user);
+        startActivity(intent);
+    }
+
+    public void onClickListenerBoutonMesInfo(View view){
+        /*Soit on supprime ce listener soit on le garde*/
+        Intent intent = new Intent(ActivityMesInformations.this, ActivityMesInformations.class);
+        /*enlever le // du dessous*/
+        //intent.putExtra("user", user);
+        startActivity(intent);
+    }
+
+    public void onClickListenerBoutonSommeil(View view){
+        /*Modifier MainActivity.class par la classe java de l'activity Sommeil)*/
+        Intent intent = new Intent(ActivityMesInformations.this, MainActivity.class);
+        /*enlever le // du dessous*/
+        //intent.putExtra("user", user);
+        startActivity(intent);
     }
 
     private void loadUserInfo() {
@@ -75,16 +276,16 @@ public class ActivityMesInformations extends AppCompatActivity {
         databaseAccess.close();
 
         String bienv="Bienvenue " + userFirstName;
-        // Mettre à jour les champs EditText avec les informations récupérées
-        editName.setText(userLastName);
+        // Mettre à jour les champs TextView avec les informations récupérées
+        nom.setText(userLastName);
         bienvenue.setText(bienv);
-        editFirstName.setText(userFirstName);
-        editAge.setText(String.valueOf(userAge));
-        editWeight.setText(String.valueOf(userWeight));
-        editHeight.setText(String.valueOf(userHeight));
+        prenom.setText(userFirstName);
+        age.setText(String.valueOf(userAge));
+        poids.setText(String.valueOf(userWeight));
+        taille.setText(String.valueOf(userHeight));
     }
 
-    private void addTextWatchers() {
+    /*private void addTextWatchers() {
         editName.setOnFocusChangeListener((v, hasFocus) -> {
             if (!hasFocus) {
                 updateUserInfo("nom", editName.getText().toString());
@@ -126,5 +327,5 @@ public class ActivityMesInformations extends AppCompatActivity {
         //SQLiteDatabase db = databaseAccess.getWritableDatabase();
         //db.execSQL(query, new String[]{value});
         databaseAccess.close(); // Ferme la connexion à la base de données
-    }
+    }*/
 }
---
+++ app/src/main/java/com/example/monsuividesante/Regex.java
@@ -0,0 +1,59 @@
+package com.example.monsuividesante;
+
+import androidx.annotation.Nullable;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class Regex {
+    public static boolean estHeureValide(String heure){
+        String expr = "([0-1][0-9]|2[0-3]):[0-5][0-9]";
+
+        return heure.matches(expr);
+    }
+
+    public static boolean estCaloriesSaisieValide(String calorie){
+        if(calorie.matches("[0-9]+"))
+            //En moyenne un humain peut consomme au maximum 3000 kcal en une journée
+            return Float.parseFloat(calorie) <= 3000;
+        return false;
+    }
+
+    public static boolean estDateDuJour(String d){
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy", Locale.FRANCE);
+        format.setLenient(false);
+
+        try {
+            Date dateATest = format.parse(d);
+
+            Date dateDuJour = format.parse(format.format(new Date()));
+
+            assert dateDuJour != null;
+            return dateDuJour.equals(dateATest);
+        } catch (Exception e) {
+            return true;
+        }
+    }
+
+
+    public static boolean estNomPrenomValide(String nom){
+        String expr = "([a-zA-Z]|-)+";
+
+        return nom.matches(expr) && nom.length()>2;
+    }
+
+    public static boolean estAgeValide(String age){
+        //122 ans = age de la personne la plus veille au monde
+        return Integer.parseInt(age)<122;
+    }
+
+    public static boolean estTailleValide(String taille){
+        //251cm = taille de la personne la plus grande au monde
+        return Integer.parseInt(taille)<251;
+    }
+
+    public static boolean estPoidsValide(String poids){
+        return Integer.parseInt(poids)<200;
+    }
+}
